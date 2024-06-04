8aa9111334b98b26d7add63557216b154b3e63d7
Angel Ramirez <angelramirezalvarez8@gmail.com>
2024-06-04 08:27:08 -0500

    Fix vercel json

---
+++ src/api/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+import { AppModule } from 'src/app.module';
+
+async function bootstrap() {
+  // somewhere in your initialization file
+
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.use(cookieParser());
+
+  const otherAllowedHeaders = ['Authorization', 'X-Requested-With'];
+
+  app.enableCors({
+    allowedHeaders: ['Content-Type', ...otherAllowedHeaders],
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
+
+  await app.listen(3000);
+}
+bootstrap();
